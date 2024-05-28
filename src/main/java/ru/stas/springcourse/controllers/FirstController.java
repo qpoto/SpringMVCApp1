13e98fb3ffd0e1ba071657239126d7f04261c2ab
qpoto <qpoto@yandex.ru>
2024-05-28 21:43:48 +0300

    После 19 урока

--- src/main/java/ru/stas/springcourse/controllers/FirstController.java
+++ src/main/java/ru/stas/springcourse/controllers/FirstController.java
@@ -3,16 +3,20 @@ package ru.stas.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String hello() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println(name + " " + surname);
         return "first/hello";
     }
+
     @GetMapping("bye")
-    public String bue() {
+    public String goodBuePage() {
         return "first/bye";
     }
 }
