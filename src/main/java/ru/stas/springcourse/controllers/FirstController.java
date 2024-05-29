4a84dbcbee862103db8da83321647d16dd674e00
Kondratev-SV <kondratevsv@intech.rshb.ru>
2024-05-29 09:43:16 +0300

    После 20 урока

--- src/main/java/ru/stas/springcourse/controllers/FirstController.java
+++ src/main/java/ru/stas/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.stas.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +11,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+        //System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("bye")
     public String goodBuePage() {
         return "first/bye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+        double result = 0;
+        switch (action) {
+            case "multi":
+                result = a * b;
+                break;
+            case "div":
+                result = (double) a / b;
+                break;
+            case "add":
+                result = a + b;
+                break;
+            case "sub":
+                result = a - b;
+                break;
+        }
+        model.addAttribute("result", result);
+        return "first/calculator";
     }
 }
