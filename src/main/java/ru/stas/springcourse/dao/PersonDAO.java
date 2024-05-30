d2571791d4a2faf9ad95ac6a70902d91d6edbc46
qpoto <qpoto@yandex.ru>
2024-05-30 17:04:15 +0300

    Полный курс После 31 урока

--- src/main/java/ru/stas/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/stas/springcourse/dao/PersonDAO.java
@@ -35,7 +35,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("insert into person values (1, ?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
+        jdbcTemplate.update("insert into person(name, age, email) values (?, ?, ?)", person.getName(), person.getAge(), person.getEmail());
     }
 
     public void update(int id, Person updatedPerson) {
